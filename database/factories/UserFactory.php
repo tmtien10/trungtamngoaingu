f37bdac58ccfb6a5af0023d1576adeb0085e74f9
tmtien10 <tmtien10@users.noreply.github.com>
2019-02-22 11:11:30 +0700

    commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,17 +6,6 @@ use App\GiangVien;
 use App\DayMonHoc;
 use App\kythi'
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 $factory->define(App\hocvien::class, function (Faker $faker) {
     return [
         'HoTenHocVien' => $faker->name,
@@ -25,7 +14,7 @@ $factory->define(App\hocvien::class, function (Faker $faker) {
         'NgaySinh'=>$faker->dateTimeBetween('1975-01-01', '2000-12-31')
     ->format('Y/m/d'),
         'SDT'=>$faker->unique()->phoneNumber,
-        'DiaChi'=>$faker->address
+        'DiaChi'=>$faker->address,
     ];
 });
 $factory->define(App\giangvien::class, function (Faker $faker) {
@@ -38,7 +27,7 @@ $factory->define(App\giangvien::class, function (Faker $faker) {
     ->format('Y/m/d'),
         'SDTGV'=>$faker->unique()->phoneNumber,
         'DiaChiGV'=>$faker->address,
-        'TinhTrang'=>1
+        'TinhTrang'=>1,
     ];
 });
 $factory->define(App\cbtt::class, function (Faker $faker) {
@@ -51,7 +40,7 @@ $factory->define(App\cbtt::class, function (Faker $faker) {
     ->format('Y/m/d'),
         'SDTNV'=>$faker->unique()->phoneNumber,
         'DiaChiNV'=>$faker->address,
-        'TinhTrang'=>1
+        'TinhTrang'=>1,
     ];
 });
 
@@ -59,7 +48,7 @@ $factory->define(App\PhieuDKHoc::class, function (Faker $faker) {
     return [
       'MaHocVien' =>hocvien::all()->unique()->random()->MaHocVien,
       'id' =>  7,
-      'TinhTrang' => 1
+      'TinhTrang' => 1,
       
     ];
 });
@@ -67,7 +56,7 @@ $factory->define(App\phieudkthi::class, function (Faker $faker) {
     return [
       'MaHocVien' =>hocvien::all()->unique()->random()->MaHocVien,
       'MaKiThi' =>  kythi::all()->unique()->random()->MaKiThi,
-      'TinhTrang' => 1
+      'TinhTrang' => 1,
       
     ];
 });
